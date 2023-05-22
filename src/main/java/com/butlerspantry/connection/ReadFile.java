02c1005530b34fbdd02057e1c93a295eb458c9cb
louisa.bieler <louisa.bieler@adevinta.com>
2023-05-22 16:47:39 +0200

    have a viewer interface and matching implementation and am developing a corresponding controller

--- src/main/java/com/butlerspantry/connection/ReadFile.java
+++ src/main/java/com/butlerspantry/connection/ReadFile.java
@@ -2,14 +2,15 @@ package main.java.com.butlerspantry.connection;
 
 import main.java.com.butlerspantry.logging.Logger;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Date;
 
 public class ReadFile {
 
-    public static File readFile(String fileName) throws Exception {
+    public static File readFile(String fileName) throws FileNotFoundException {
         File newFileObject = new File(fileName);
         if (!newFileObject.isFile()) {
-            throw new Exception("No such file found. Please check input!");
+            throw new FileNotFoundException("No such file found. Please check input!");
         }
         String logSuccess = "readFile Successful on " + new Date();
         Logger.logNow(logSuccess);
